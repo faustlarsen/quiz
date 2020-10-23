7fa8b5139277744539e904d7a0992c5dcb73e04c
faustlarsen <faustlarsen@gmail.com>
2020-10-23 16:30:45 -0500

    add code that tries to add numeric value to conditions

--- js/scripts.js
+++ js/scripts.js
@@ -6,68 +6,51 @@ $(document).ready(function() {
   const answer3 = $("input:radio[name=q3]:checked").val();
   const answer4 = $("input:radio[name=q4]:checked").val();
   const answer5 = $("input:radio[name=q5]:checked").val();
-  const name = $("#name").val();
 
-/* tried to upper case first letters of two names. Only first works"*/
+
+  const name = $("#name").val();
   const upper = name.split(" ");
   for (let i = 0; i < upper.length; i++) {
     upper[i] = upper[i][0].toUpperCase() + upper[i].substr(1);
   }
   let final = upper.join(" ");
 
-/* this works to cap first letter of one name (string) only 
-  const upper = name.slice(0,1).toUpperCase();
-  const lower = name.slice(1,name.length).toLowerCase();
-  const finalName = upper + lower;
-*/
-
   if (answer1 === '1' && answer2 === '4' && answer3 === '7' && answer4 === '10' && answer5 === '13') {
-    $("#ruby").hide();
-    $("#french").hide();
-    $("#swift").hide();
-    $("#trythis").hide();
-    $("#image").hide();
-    $("#java").show();
+    $("#ruby").fadeOut();
+    $("#french").fadeOut();
+    $("#swift").fadeOut();
+    $("#trythis").fadeOut();
+    $("#image").fadeOut();
+    $("#java").fadeIn();
 
   } else if (answer1 === '2' && answer2 === '5' && answer3 === '8' && answer4 === '11' && answer5 === '14') {
-    $("#java").hide();
-    $("#french").hide();
-    $("#swift").hide();
-    $("#trythis").hide();
-    $("#ruby").show();
+    $("#java").fadeOut();
+    $("#french").fadeOut();
+    $("#swift").fadeOut();
+    $("#trythis").fadeOut();
+    $("#ruby").fadeIn();
 
   } else if (answer1 === '3' && answer2 === '6' && answer3 === '9' && answer4 === '12' && answer5 === '15') {
-    $("#ruby").hide();
-    $("#french").hide();
-    $("#java").hide();
-    $("#trythis").hide();
-    $("#swift").show();
+    $("#ruby").fadeOut();
+    $("#french").fadeOut();
+    $("#java").fadeOut();
+    $("#trythis").fadeOut();
+    $("#swift").fadeIn();
 
   } else if (answer1 === '2' && answer2 === '4' && answer3 === '9' && answer4 === '10' && answer5 === '13') {
-    $("#ruby").hide();
-    $("#swift").hide();
-    $("#java").hide();
-    $("#trythis").hide();
-    $("#french").show();
+    $("#ruby").fadeOut();
+    $("#swift").fadeOut();
+    $("#java").fadeOut();
+    $("#trythis").fadeOut();
+    $("#french").fadeIn();
 
   } else {
-    $("#ruby").hide();
-    $("#java").hide();
-    $("#swift").hide();
-    $("#french").hide();
-    $("#trythis").show();
+    $("#ruby").fadeOut();
+    $("#java").fadeOut();
+    $("#swift").fadeOut();
+    $("#french").fadeOut();
+    $("#trythis").fadeIn();
   }
-  
-
-  /* red doesnt disappear
-  if ( name !== undefined && answer1 !== undefined && answer2 !== undefined && answer3 !== undefined && answer4 !== undefined && answer5 !== undefined) {
-    $(".form-control").addClass("red") 
-    $("#image").show();
-  } else {
-    $(".form-control").removeClass("red");
-    $("#image").hide();
-  }
-  */
 
   $(".firstName").text(final);
   $("#result").show();
@@ -79,3 +62,27 @@ $(document).ready(function() {
 
 
 
+/*  tried to add numeric value
+let results = $("#results");
+  let score = 0;
+  if (answer1 == "dd") {
+    score += 2
+  } else {
+    score += 1
+  }
+
+  if(score >= 2){
+    results.show("qw");
+  } else {
+    results.show("fg");
+  }
+  
+
+
+/* tried to have image appear if no value entered
+  if (name !== undefined) {
+    $("#image").fadeIn();
+  } else {
+    $("#image").fadeOut();
+  }
+*/
