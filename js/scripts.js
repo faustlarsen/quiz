0bb39b6a2adafe77ae49f687bb27f3eca94e44c7
faustlarsen <faustlarsen@gmail.com>
2020-10-23 13:39:59 -0500

    highlight name input red if its empty

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $("#form").submit(function(event){
-
+    
   const answer1 = $("input:radio[name=q1]:checked").val();
   const answer2 = $("input:radio[name=q2]:checked").val();
   const answer3 = $("input:radio[name=q3]:checked").val();
@@ -9,16 +9,16 @@ $(document).ready(function() {
   const name = $("#name").val();
 
   if (answer1 === '1' && answer2 === '4' && answer3 === '7' && answer4 === '10' && answer5 === '13') {
+    $("#ruby").hide();
+    $("#french").hide();
+    $("#swift").hide();$
+    ("#java").show();
+
+  } else if (answer1 === '2' && answer2 === '5' && answer3 === '8' && answer4 === '11' && answer5 === '14') {
     $("#java").hide();
     $("#french").hide();
     $("#swift").hide();
     $("#ruby").show();
-
-  } else if (answer1 === '2' && answer2 === '5' && answer3 === '8' && answer4 === '11' && answer5 === '14') {
-    $("#ruby").hide();
-    $("#french").hide();
-    $("#swift").hide();
-    $("#java").show();
 
   } else if (answer1 === '3' && answer2 === '6' && answer3 === '9' && answer4 === '12' && answer5 === '15') {
     $("#ruby").hide();
@@ -28,9 +28,15 @@ $(document).ready(function() {
 
   } else {
     $("#ruby").hide();
-    $("#french").hide();
+    $("#java").hide();
     $("#swift").hide();
     $("#french").show();
+  }
+  
+  if (name !== undefined) {
+    $(".form-control").addClass("red");
+  } else {
+    $(".form-control").removeClass("red");
   }
 
   $(".firstName").text(name);
@@ -39,3 +45,7 @@ $(document).ready(function() {
   event.preventDefault();
   });
 });
+
+
+
+
